9dfd953a5726b1639b33c9885ece083e6447a237
Ivailo <papazov@vmandco.com>
2014-10-02 15:38:16 +0300

    show user profile not translate

--- app/Resources/FOSUserBundle/views/Profile/show_content.html.php
+++ app/Resources/FOSUserBundle/views/Profile/show_content.html.php
@@ -1,5 +1,5 @@
 <div class="fos_user_user_show">
-    <p>{{ 'profile.show.username'|trans({}, 'FOSUserBundle') }}: {{ user.username }}</p>
-    <p>{{ 'profile.show.email'|trans({}, 'FOSUserBundle') }}: {{ user.email }}</p>
-    <p><a href="{{ path('fos_user_profile_edit') }}">Edit</a></p>
+    <p>{{ 'profile.show.username'|trans }}: {{ user.username }}</p>
+    <p>{{ 'profile.show.email'|trans }}: {{ user.email }}</p>
+    <p><a href="{{ path('fos_user_profile_edit') }}">{{'profile.show.edit'|trans}}</a></p>
 </div>
